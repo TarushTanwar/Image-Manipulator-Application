b2a6cd40ed7152a1b40d5a5c31a1daed64fe789c
TarushTanwar <141051106+TarushTanwar@users.noreply.github.com>
2023-11-08 15:48:51 +0530

    Add files via upload

---
+++ image_manipulator.py
@@ -0,0 +1,37 @@
+from PIL import Image
+
+def open_image(file_path):
+    try:
+        image = Image.open(file_path)
+        return image
+    except Exception as e:
+        print("Error opening the image:", str(e))
+        return None
+
+def save_image(image, file_path):
+    try:
+        image.save(file_path)
+        print("Image saved successfully.")
+    except Exception as e:
+        print("Error saving the image:", str(e))
+
+def grayscale_image(image):
+    try:
+        grayscale = image.convert('L')
+        return grayscale
+    except Exception as e:
+        print("Error converting the image to grayscale:", str(e))
+        return None
+
+def main():
+    file_path = 'c:/Users/Dell/Desktop/images.jpg'
+    image = open_image(file_path)
+
+    if image:
+        grayscale = grayscale_image(image)
+
+        if grayscale:
+            save_image(grayscale, 'grayscale_image.jpg')
+
+if __name__ == "__main__":
+    main()
